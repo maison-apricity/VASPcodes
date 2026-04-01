38ee1b573ae3427cf7cd0e178308638b4a152bf3
998.001 <42725640+maison-apricity@users.noreply.github.com>
2026-04-01 14:01:28 +0900

    Delete VASPneb/__VTSTscripts directory

--- VASPneb/__VTSTscripts/am-cron-wrapper.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -l
-
-if [[ ! -d $1 ]]; then
-    echo "$1 is not a directory"
-    exit 1
-fi
-
-cd $1
-automagician.py -rb
--- VASPneb/__VTSTscripts/insta-submit-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -l                                                                                                                                                                                              
-
-TMPFILE=automagician-insta-submit-$(date +%s)
-crontab -l > $TMPFILE
-echo "* * * * * $(which insta-submit.sh)" >> $TMPFILE
-crontab $TMPFILE
-rm $TMPFILE
